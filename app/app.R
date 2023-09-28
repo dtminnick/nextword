869dfae414a343660652d643cea03a80351b5df6
Donnie Minnick <donnie.minnick@gmail.com>
2023-09-28 18:02:53 -0400

    Shiny app updates

--- app/app.R
+++ app/app.R
@@ -1,6 +1,10 @@
 
+library("ggplot2")
+library("plotly")
 library("shiny")
 library("shinyBS")
+
+source("../R/ngram_predictor.R")
 
 methods <- c("Ngram", "Katz Backoff")
 
@@ -12,9 +16,9 @@ ui <- fluidPage(
 
                 sidebarPanel(
 
-                        selectInput("method", "Prediction Method", methods),
+                        selectInput("model", "Prediction Model", methods),
 
-                        bsTooltip("method", "Select the method used to make a prediction.",
+                        bsTooltip("model", "Select the method used to make a prediction.",
                                   "right", options = list(container = "body")),
 
                         textInput("input_phrase", "Input Phrase"),
@@ -42,14 +46,20 @@ ui <- fluidPage(
                         bsTooltip("min_count", "Select the minimum number of ngrams to return.",
                                   "right", options = list(container = "body")),
 
-                        actionButton("predict", "Run Prediction", class = "btn-primary"),
+                        actionButton("predict", "Generate Prediction", class = "btn-primary"),
 
-                        bsTooltip("predict", "Click to run the prediction model.",
+                        bsTooltip("predict", "Click to run the selected model and generate a prediction.",
                                   "right", options = list(container = "body")),
 
                 ), # sidebarPanel
 
                 mainPanel(
+
+                        br(),
+
+                        strong("Next Word Predicted", align = "left"),
+
+                        verbatimTextOutput("predicted_word", placeholder = TRUE),
 
                         tabsetPanel(
 
@@ -61,9 +71,15 @@ ui <- fluidPage(
 
                                 ),
 
-                                tabPanel("Plot"
+                                tabPanel("Plot",
 
+                                        br(),
 
+                                        p("This plot shows the ten words determined by the algorithm to have the
+                                          highest probability of being the next word in the phrase provided by the
+                                          user. Words are plotted in decsending order of probability."),
+
+                                        plotlyOutput("plot"),
 
                                 ),
 
@@ -95,7 +111,42 @@ server <- function(input, output, session) {
 
         })
 
-        output$data <- renderTable(input$source)
+        # output$data <- renderTable(input$source)
+
+
+        prediction <- eventReactive(input$predict, {
+
+                source <- read_source(as.character(input$source$datapath))
+
+                ngram_predictor(input$input_phrase,
+                                source,
+                                input$n,
+                                input$dec_pos,
+                                input$min_count)
+
+        })
+
+        # output$predicted_word <- renderPrint(get_predicted_word(prediction))
+
+        dataTable <- eventReactive(input$predict, {
+
+                prediction()
+
+        })
+
+        output$data <- renderDataTable(dataTable())
+
+        dataPlot <- eventReactive(input$predict, {
+
+                prediction()
+
+        })
+
+        output$plot <- renderPlotly({
+
+                ggplotly(get_plot(dataPlot()))
+
+        })
 
 }
 
